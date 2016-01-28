2e2e5318804f0ed7f9713536bc545f5ad99594a8
Adam Avramov <fallenblood@gmail.com>
2016-01-28 21:58:11 +0200

    disable tripfaggery for now

--- client/templates/status.js
+++ client/templates/status.js
@@ -4,7 +4,7 @@
 
   return $.h(".status",
     [ $.h(".statusItem", [ $.h("input#username", { type: "text",     placeholder: "onanimus" }) ])
-    , $.h(".statusItem", [ $.h("input#password", { type: "password", placeholder: "secret (for tripcode)" }) ])
+    //, $.h(".statusItem", [ $.h("input#password", { type: "password", placeholder: "secret (for tripcode)" }) ])
     , $.h(".statusItem", (state.server ? "" : "not ") + "connected to server")
     , $.h(".statusItem", (p2p          ? "" : "not ") + "connected to peer broker")
     , $.h(".statusItem", "connected to " + state.p2p.peers.length + " peers") ])
