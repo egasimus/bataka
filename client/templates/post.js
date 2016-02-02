d13667fbb1022feca596ca0b1c02150642660968
Adam Avramov <fallenblood@gmail.com>
2016-02-02 03:55:50 +0200

    update styles and templates, write intro

--- client/templates/post.js
+++ client/templates/post.js
@@ -2,13 +2,14 @@
 
   var media = post.media ? _.media(post) : null
     , body = $.h(".postBody",
-      [ $.h("img.postUserPic", { src: "http://www.placecage.com/c/32/32" })
-      , $.h("span.postUserName", post.user || $.h("em", "onanimus"))
-      , $.h("span.postText", post.text) 
-      , $.h("div",
-        [ $.h("a.postDate", { href: "#" + post.id }, String(post.time))
-        , post.trip ? $.h("span.postTripCode", " - " + post.trip) : null
-        ]) ])
+      [ //$.h("img.postUserPic", { src: "http://www.placecage.com/c/32/32" })
+        $.h(".postUserName", post.user || $.h("em", "onanimus"))
+      , $.h("span.postText", post.text)
+      , $.h("span.postInfo",
+          [ $.h("a.postDate", { href: "#" + post.id }, String(post.time))
+          , post.trip ? " - " : null
+          , post.trip ? $.h("span.postTripCode", post.trip) : null ])
+      ])
 
   return $.h(".post#post_" + post.id, [media, body]);
 
--- server/state.js
+++ server/state.js
@@ -1,10 +1,40 @@
-$.lib.riko.M({
+(function () {
 
-  threads:
-    {},
+  var tatuiral = require('fs').readFileSync(
+    require("path").resolve(__dirname, "../README"), 'utf8').trim().split("\n\n\n");
 
-  people:
-    0
+  return $.lib.riko.M({
 
-})
+    threads:
+      { "tatuiral":
+        { id: "tatuiral"
+        , posts:
+          [ { id: "firstPostEver"
+            , user: "iniziador"
+            , trip: "TRIPKOD: IBO VAISTENU"
+            , time: "DATA: FNA4ALOTO"
+            , mediaType: "ipfs"
+            , media: "QmUHAZMsZ5jd6mvtx5QW1HKs9BChyk327joDB2sW2bkhod"
+            , text: tatuiral[0] }
+          , { id: "firstPostEver2"
+            , user: "iniziador"
+            , trip: "TRIPKOD: IBO VAISTENU"
+            , time: "DATA: FNA4ALOTO"
+            , mediaType: "ipfs"
+            , media: "Qmai5tAvGrgUQSukU5ZTvsknbFnCYCwJu41pHngeS8aaHn"
+            , text: tatuiral[1] }
+          , { id: "firstPostEver3"
+            , user: "iniziador"
+            , trip: "TRIPKOD: IBO VAISTENU"
+            , time: "DATA: FNA4ALOTO"
+            , mediaType: "ipfs"
+            , media: "QmPkUDfoF1Atdn6dJfVvseGk4oM38GgLt4AgeGhh1FHgK2"
+            , text: tatuiral[2] }
+            ]} },
 
+    people:
+      0
+
+  })
+
+})()
