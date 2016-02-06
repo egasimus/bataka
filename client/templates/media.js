7285144cfeb5475cc1bf7197b14ea105eaaa1cb9
Adam Avramov <fallenblood@gmail.com>
2016-02-06 14:39:48 +0200

    render media again

--- client/templates/media.js
+++ client/templates/media.js
@@ -1,9 +1,11 @@
 (function (media) {
 
   if (media && media.src) {
-    var service = _.mediaServices[media.service] || _.mediaServices["default"]
+    var service = _.mediaServices[media.service]
+          || _.mediaServices["default"]
       , url     = service(media.src)
-      , type    = _.mediaTypes[media.type] || _.mediaTypes["download"]
+      , type    = _.mediaTypes[media.type.split('/')[0]]
+          || _.mediaTypes["download"]
       , media   = type(url);
     return media;
   }
