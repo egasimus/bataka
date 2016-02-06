5c1291201fc655d835dfffe6df46d4e4aa1a000c
Adam Avramov <fallenblood@gmail.com>
2016-02-06 16:03:31 +0200

    show post info on same row as username

--- client/templates/post.js
+++ client/templates/post.js
@@ -3,12 +3,12 @@
   var media = post.media ? $.h(".postMedia", _.media(post.media)) : null
     , body = $.h(".postBody",
       [ //$.h("img.postUserPic", { src: "http://www.placecage.com/c/32/32" })
-        $.h(".postUserName", post.user || $.h("em", "onanimus"))
-      , $.h(".postText", { innerHTML: $.util.markdown(post.text) })
-      , $.h("span.postInfo",
-          [ $.h("a.postDate", { href: "#post_" + post.id }, String(post.time))
+      , $.h(".postInfo",
+          [ $.h(".postUserName", post.user || $.h("em", "onanimus"))
+          , $.h("a.postDate", { href: "#post_" + post.id }, String(post.time))
           , post.trip ? " - " : null
           , post.trip ? $.h("span.postTripCode", post.trip) : null ])
+      , $.h(".postText", { innerHTML: $.util.markdown(post.text) })
       ])
 
   return $.h(".post#post_" + post.id, [media, body]);
