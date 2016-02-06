90b6b407ccbf6e462b24e7c279a66e40eec29a0f
Adam Avramov <fallenblood@gmail.com>
2016-02-06 19:47:15 +0200

    remove autoreloading stuff for now

--- client/main.js
+++ client/main.js
@@ -6,19 +6,12 @@
 
   // insert css first to prevent fouc
   var css = $.util.insertCss($.style);
-  app.nodes['style.styl'].events.on('edited', function () {
-    css.parentElement.removeChild(css);
-    css = $.util.insertCss($.style);
-  });
 
   // start vdom main loop, initialize lazy image loader
   var view = $.lib.riko.V($.state, $.templates.app);
   //$.util.lazyLoader.init();
   document.body.innerHTML = "";
   document.body.appendChild(view.target);
-  app.nodes['templates'].events.after('edited', function () { // TODO
-    view.update($.state());
-  });
 
   return view;
 
