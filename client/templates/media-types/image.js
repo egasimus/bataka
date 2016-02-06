37e3d32b686f8abd120f2c55ed90dec6741aea1a
Adam Avramov <fallenblood@gmail.com>
2016-02-06 17:28:07 +0200

    add option for lazy loading

--- client/templates/media-types/image.js
+++ client/templates/media-types/image.js
@@ -1,5 +1,7 @@
 (function (url) {
   return $.h("a", { href: url, target: "_blank" },
     $.h("img.postMediaImage.lazyload",
-      { attributes: { "data-src": url } }));
+      $.options.lazyLoad
+        ? { attributes: { "data-src": url } }
+        : { src: url }));
 })
