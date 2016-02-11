4c9695c895f4ea4e3e7ee635f6c93f5c31080d69
Adam Avramov <fallenblood@gmail.com>
2016-02-12 00:57:43 +0200

    mark posts as seen while scrolling past

--- client/events/scroll.js
+++ client/events/scroll.js
@@ -1,5 +1,21 @@
 (function () {
 
-  console.log('scroll', window.scrollY)
+  Array.prototype.forEach.call(
+    document.getElementsByClassName('notSeen post'), check);
+
+  function check (post) {
+    var id   = post.parentElement.id.slice(7)
+      , rect = post.getBoundingClientRect();
+    if (seeing(rect)) {
+      setTimeout(function () {
+        if (seeing(rect)) $.emit('seen', id)();
+      }, 1000);
+    }
+  }
+
+  function seeing (rect) {
+    return ((rect.top > 0 && rect.bottom < window.innerHeight) ||
+            (rect.top < 0 && rect.bottom > window.innerHeight))
+  }
 
 })
